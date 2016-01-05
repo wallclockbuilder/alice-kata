a1144803c9cb5deecbc4453da8bbe882de2a7719
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2016-01-05 13:26:20 -0700

    Fix names. Fix functions. Add comments.
    By convention names should represent what they return.
    Functions should be mostly pure(that is without side effects).
    Add comments that explain the WHY when necessary.

--- main.go
+++ main.go
@@ -23,6 +23,8 @@ func (l byCount) Less(i, j int) bool { return l[i].count < l[j].count }
 func (l byCount) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
 
 func insert(dict map[string]int, word string) map[string]int {
+	// insert adds the input word to the input dictionary
+	// and return the modified dictionary.
 	if _, ok := dict[word]; !ok {
 		dict[word] = 1
 		return dict
@@ -31,29 +33,31 @@ func insert(dict map[string]int, word string) map[string]int {
 	return dict
 }
 
-func extractWordsFromLine(s string) []string {
+func words(s string) []string {
+	// words returns a slice of the words from the input string.
 	regex := regexp.MustCompile("\\w+")
 	words := regex.FindAllString(s, -1)
 	return words
 }
 
 func assertNil(err error) {
+	// assertNil makes sure theres no error. log panics when there is.
 	if err != nil {
 		log.Panic(err)
 	}
 }
 
-func convertToList(m map[string]int) []word {
-	// slice converts a map to a slice of words
-	list := []word{}
+func list(m map[string]int) []word {
+	// list converts the supplied map to a slice of words
+	var list []word
 	for k, v := range m {
 		list = append(list, word{k, v})
 	}
 	return list
 }
 
-func addWordsToDictionary(dict map[string]int, words []string) map[string]int {
-	// addaddWordsToDictionary adds the words in the supplied list to the
+func dictionary(dict map[string]int, words []string) map[string]int {
+	// dictionary adds the words in the supplied list to the
 	// supplied dictionary and returns the dictionary.
 	for _, w := range words {
 		dict = insert(dict, w)
@@ -61,31 +65,33 @@ func addWordsToDictionary(dict map[string]int, words []string) map[string]int {
 	return dict
 }
 
-func mostFrequent(wordFrequencyList []word, n int) []word {
+func first(wordFrequencyList []word, n int) []word {
+	// first returns the first n words from supplied list.
 	return wordFrequencyList[:n]
 }
 
 func main() {
 	dict := make(map[string]int)
-	wordFrequencyList := []word{}
 	f, err := os.Open("alice-in-wonderland.txt")
-
 	assertNil(err)
 
 	r := bufio.NewReader(f)
 	for {
-		switch s, err := r.ReadString('\n'); err {
+		switch line, err := r.ReadString('\n'); err {
 		case nil:
-			words := extractWordsFromLine(s)
-			dict = addWordsToDictionary(dict, words)
+			dict = dictionary(dict, words(line))
 		case io.EOF:
 			// convert dict to slice for sorting in O(n) time.
 			// maps are horrible for sorting because the go
 			// runtime randomizes map iteration order.
 			// So we use a different data structure(slice) to maintain order.
-			wordFrequencyList = convertToList(dict)
+			wordFrequencyList := list(dict)
+
+			// sort orders the slice from smallest to biggest
+			// sort.reverse ensures an ordering from big to small
 			sort.Sort(sort.Reverse(byCount(wordFrequencyList)))
-			fmt.Println(mostFrequent(wordFrequencyList, 7))
+
+			fmt.Println(first(wordFrequencyList, 7))
 			return
 		default:
 			return
