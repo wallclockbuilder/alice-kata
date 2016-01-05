511d777bdf627040a3bec24d8d7cc8a134166dd7
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2016-01-05 12:45:01 -0700

    Remove useless comments.

--- main.go
+++ main.go
@@ -37,38 +37,56 @@ func extractWordsFromLine(s string) []string {
 	return words
 }
 
-func main() {
-	dict := make(map[string]int)
-	list := []word{}
-	f, err := os.Open("alice-in-wonderland.txt")
+func assertNil(err error) {
 	if err != nil {
 		log.Panic(err)
 	}
-	r := bufio.NewReader(f)
+}
 
+func convertToList(m map[string]int) []word {
+	// slice converts a map to a slice of words
+	list := []word{}
+	for k, v := range m {
+		list = append(list, word{k, v})
+	}
+	return list
+}
+
+func addWordsToDictionary(dict map[string]int, words []string) map[string]int {
+	// addaddWordsToDictionary adds the words in the supplied list to the
+	// supplied dictionary and returns the dictionary.
+	for _, w := range words {
+		dict = insert(dict, w)
+	}
+	return dict
+}
+
+func mostFrequent(wordFrequencyList []word, n int) []word {
+	return wordFrequencyList[:n]
+}
+
+func main() {
+	dict := make(map[string]int)
+	wordFrequencyList := []word{}
+	f, err := os.Open("alice-in-wonderland.txt")
+
+	assertNil(err)
+
+	r := bufio.NewReader(f)
 	for {
 		switch s, err := r.ReadString('\n'); err {
 		case nil:
 			words := extractWordsFromLine(s)
-
-			for _, w := range words {
-				dict = insert(dict, w)
-			}
-
+			dict = addWordsToDictionary(dict, words)
 		case io.EOF:
-			fmt.Println("EOF")
-			//convert dict to slice for sorting in O(n) time.
-			// slices are horrible for sorting
-			for k, v := range dict {
-				list = append(list, word{k, v})
-			}
-
-			sort.Sort(sort.Reverse(byCount(list)))
-
-			// print out top 7 most common words
-			fmt.Println(list[:7])
+			// convert dict to slice for sorting in O(n) time.
+			// maps are horrible for sorting because the go
+			// runtime randomizes map iteration order.
+			// So we use a different data structure(slice) to maintain order.
+			wordFrequencyList = convertToList(dict)
+			sort.Sort(sort.Reverse(byCount(wordFrequencyList)))
+			fmt.Println(mostFrequent(wordFrequencyList, 7))
 			return
-
 		default:
 			return
 		}
